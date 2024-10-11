2cf59fb587ee2723ecd83429ef10101b43c4d8a3
Sedenu <aloayecharles@gmail.com>
2024-10-11 16:13:48 +0100

    Update grade.js

--- js/grade.js
+++ js/grade.js
@@ -1,4 +1,5 @@
 let gradeChecker = () => {
+    
     let thresh_hold = 100;
 
     let grade = document.querySelector('grade').value;
@@ -6,38 +7,47 @@ let gradeChecker = () => {
     let success = document.querySelector('success');
     let warning = document.querySelector('warning');
 
-    if(grade && grade > thresh_hold){
+    if(grade && grade > thresh_hold)
+    {
         error.style.display ='none';
         success.style.display ='none';
         warning.style.display ='block';
         warning.innerHTML = 'Please enter score between 1-100';
     }
-    else if(grade && grade <= thresh_hold){
+        
+    else if(grade && grade <= thresh_hold)
+    {
         success.style.display = 'block';
         warning.style.display ='none';
         error.style.display ='none';
 
-        if(grade >= 70) {
+        if(grade >= 70) 
+        {
             success.innerHTML = 'A';
         }
-        else if(grade >= 60){
+        else if(grade >= 60)
+        {
             success.innerHTML = 'B';
         }
-        else if(grade >= 50){
+        else if(grade >= 50)
+        {
             success.innerHTML = 'C';
         }
-        else if(grade >= 45){
+        else if(grade >= 45)
+        {
             success.innerHTML = 'D';
         }
-        else if(grade >= 40){
+        else if(grade >= 40)
+        {
             success.innerHTML = 'E';
-        } else {
+        } else 
+        {
             success.innerHTML = 'F';
 
-        }
+         }
 
-        // success.innerHTML=grade;
-    } else {
+    } else 
+    {
         error.style.display = 'block';
         warning.style.display ='none';
         success.style.display ='none';
